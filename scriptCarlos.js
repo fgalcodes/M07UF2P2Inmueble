736e3a4187abd556ef9b7d4399c1937d0f843839
Carlos7e6 <carlos.gomez.7e6@itb.cat>
2023-03-21 21:53:09 +0100

    Commit generico al empezaer

---
+++ scriptCarlos.js
@@ -0,0 +1,25 @@
+var casa = new Casa
+("Carrer de l'amargura",
+35,
+"654gf",
+500,
+"url",
+"en venta",
+[0,1],
+2020,
+false,
+0,
+true);
+
+var piso = new Piso(
+    "Carrer de la Alegria",
+    50,
+    "654ghf",
+    600,
+    "url",
+    "en venta",
+    [54,65],
+    40,
+    [1,2],
+    false
+);
