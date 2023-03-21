b89698e6e2eeb0207c559b2f679dcc31e1d9ab60
Carlos7e6 <carlos.gomez.7e6@itb.cat>
2023-03-21 10:07:56 +0100

    Actualizacion de inmuebles

--- obj/inmueble.js
+++ obj/inmueble.js
@@ -33,4 +33,71 @@ class Piso extends Inmueble {
     }
 }
 
+class Casa{
+    nParedesColin;
+    unifamiliar;
+    nBanyos;
+    nHabitaciones
+
+    extras = new Map();
+    jardin;
+    tamanyoJardin;
+    piscina;
+}
+
+class Local{
+
+    nVentanas;
+    persianaMetalica;
+
+    constructor(vent,pers){
+        this.nVentanas = vent;
+        this.persianaMetalica = pers;
+    }
+
+    variaPrecioLocal(){
+
+    }
+
+}
+
+class Restauracion extends Local
+{
+    equipacion = new Map();
+
+    constructor(vent,pers,extract,cafet,mobili){
+        super(vent,pers);
+        this.equipacion.set("Extractor",extract);
+        this.equipacion.set("Cafetera",cafet);
+        this.equipacion.set("Mobiliario",mobili);
+    }
+}
+
+class Industrial extends Local {
+
+    tipoSuelo;
+    persianaMetalica;
+
+    constructor(vent,pers,tipSuel,persMetal){
+        super(vent,pers);
+        this.tipoSuelo = tipSuel;
+        this.persianaMetalica = persMetal;
+    }
+
+    cobroImpuestos(){
+
+    }
+}
+
+class Comercio extends Local{
+
+    accesible;
+
+    constructor(vent,pers,accs){
+        super(vent, pers);
+        this.accesible = accs;
+    }
+    
+}
+
 
