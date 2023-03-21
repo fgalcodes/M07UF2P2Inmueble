f97cdbdb794fd9adcf8727299789fc51fd84e0c1
fabian <fabian.galindo.7e6@itb.cat>
2023-03-21 09:46:39 +0100

    update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var inmueble = new Inmueble();
 var piso = new Piso();
 
-console.log(inmueble);
-console.log(piso);
+// console.log(inmueble);
+// console.log(piso);
 
 piso.direccion = "casa aigua itb";
 
-console.log(piso);
+// console.log(piso);
 
 
 var inmuebles = new Cerebro();
@@ -14,12 +14,13 @@ var inmuebles = new Cerebro();
 inmuebles.altaVivienda(piso);
 inmuebles.altaVivienda(inmueble);
 
-console.log(inmuebles);
+// console.log(inmuebles);
 
 const formAgregar = document.getElementById("buscador-inmueble");
-formAgregar.addEventListener("buscar", (e) => {
+formAgregar.addEventListener("submit", (e) => {
     e.preventDefault();
     var nuevoInmueble = new Piso();
     nuevoInmueble.direccion = document.getElementById("direccion-inmueble").value
-    console.log(discografia);
+
+    console.log(inmuebles.inmuebles[0]);
 });
