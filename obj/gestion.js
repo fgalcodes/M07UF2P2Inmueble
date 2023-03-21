15e58d8ca6bea20f1b9625424b7ca751e58120e1
fabian <fabian.galindo.7e6@itb.cat>
2023-03-21 18:18:30 +0100

    commit

--- obj/gestion.js
+++ obj/gestion.js
@@ -1,8 +1,13 @@
 class Gestion {
     inmuebles = [];
 
+    // quizas sirve
+    
+    pisos = [];
+    casas = [];
+    locales = [];
+
     altaVivienda(inmueble) {
         this.inmuebles.push(inmueble);
     }
-
 }
