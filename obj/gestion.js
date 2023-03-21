a7d9f3d66bf8b23ae9c59cf11bb0d5b4e6d3b73f
Carlos7e6 <carlos.gomez.7e6@itb.cat>
2023-03-21 23:31:26 +0100

    Nuevas funcionalidades

--- obj/gestion.js
+++ obj/gestion.js
@@ -1,13 +1,34 @@
 class Gestion {
+
     inmuebles = [];
 
-    // quizas sirve
-    
-    pisos = [];
-    casas = [];
-    locales = [];
-
-    altaVivienda(inmueble) {
+    altaVivienda(inmueble) 
+    {
         this.inmuebles.push(inmueble);
+    }
+
+    bajaVivienda(refCatastral)
+    {
+       for(let i; i < this.inmuebles.length; i++)
+       {
+            if(this.inmuebles[i].refCatastral == refCatastral)
+            {
+                this.inmuebles.splice(i,1);
+            }
+       } 
+    }
+    desactivarInmueble(refCatastral)
+    {
+        for(let inmueble of this.inmuebles)
+        {
+            if(inmueble.refCatastral == refCatastral){
+                inmueble.show = false;
+            }
+        }
+        //reload del section que contendria los inmuebles
+    }
+    modificarInmueble(refCatastral)
+    {
+
     }
 }
