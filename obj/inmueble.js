4743e51088dc557e5cef1576cbc22cee7238b722
Carlos7e6 <carlos.gomez.7e6@itb.cat>
2023-03-21 13:11:05 +0100

    Constructores ready

--- obj/inmueble.js
+++ obj/inmueble.js
@@ -8,68 +8,72 @@ class Inmueble {
     coordenadas;
     anyos;
 
-    constructor() {
-        this.direccion = "";
-        this.mCuadrados = 0;
-        this.refCatastral = "";
-        this.precioBase = 0;
-        this.estado = "";
-        this.coordenadas = [];
-        this.anyos = 0;       
+    constructor(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos) {
+        this.direccion = direccion;
+        this.mCuadrados = mCuadrados;
+        this.refCatastral = refCatastral;
+        this.precioBase = precioBase;
+        this.foto = foto;
+        this.estado = estado;
+        this.coordenadas = coordenadas;
+        this.anyos = anyos;
     }
 }
 
 class Piso extends Inmueble {
-    planta;
-    portal;
+
+    plantaPuerta
     terraza;
 
-    constructor() {
-        super();
-        this.planta = 0;
-        this.portal = "";
-        this.terraza = false;
+    constructor(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos, plantaPuerta, terraza) {
+        super(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos);
+        this.plantaPuerta = plantaPuerta;
+        this.terraza = terraza;
 
     }
 }
 
-class Casa{
+class Casa extends Inmueble {
     nParedesColin;
     unifamiliar;
     nBanyos;
     nHabitaciones
 
     extras = new Map();
-    jardin;
-    tamanyoJardin;
-    piscina;
+
+    constructor(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos, jardin, tamanyoJardin, piscina) {
+        super(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos);
+        this.extras.set("Jardin", jardin);
+        this.extras.set("MetrosJardin", tamanyoJardin);
+        this.extras.set("Piscina", piscina);
+    }
 }
 
-class Local{
+class Local extends Inmueble {
 
     nVentanas;
     persianaMetalica;
 
-    constructor(vent,pers){
+    constructor(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos, vent, pers) {
+        super(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos);
         this.nVentanas = vent;
         this.persianaMetalica = pers;
     }
 
-    variaPrecioLocal(){
+    variaPrecioLocal() {
 
     }
 
 }
 
-class Restauracion extends Local
-{
+class Restauracion extends Local {
     equipacion = new Map();
 
-    constructor(vent,pers,extract,cafet,mobili){
-        super(vent,pers);
-        this.equipacion.set("Extractor",extract);
-        this.equipacion.set("Cafetera",cafet);
-        this.equipacion.set("Mobiliario",mobili);
+    constructor(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos, vent, pers, vent, pers, extract, cafet, mobili) {
+        super(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos, vent, pers);
+        this.equipacion.set("Extractor", extract);
+        this.equipacion.set("Cafetera", cafet);
+        this.equipacion.set("Mobiliario", mobili);
     }
 }
 
@@ -78,26 +82,26 @@ class Industrial extends Local {
     tipoSuelo;
     persianaMetalica;
 
-    constructor(vent,pers,tipSuel,persMetal){
-        super(vent,pers);
+    constructor(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos, vent, pers, tipSuel, persMetal) {
+        super(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos, vent, pers);
         this.tipoSuelo = tipSuel;
         this.persianaMetalica = persMetal;
     }
 
-    cobroImpuestos(){
+    cobroImpuestos() {
 
     }
 }
 
-class Comercio extends Local{
+class Comercio extends Local {
 
     accesible;
 
-    constructor(vent,pers,accs){
-        super(vent, pers);
+    constructor(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos,vent, pers, accs) {
+        super(direccion, mCuadrados, refCatastral, precioBase, foto, estado, coordenadas, anyos, vent, pers);
         this.accesible = accs;
     }
-    
+
 }
 
 
