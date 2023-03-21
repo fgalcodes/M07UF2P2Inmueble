e1597fdbe50e9615ab9bb67dd39ed65cbdd95957
Carlos7e6 <carlos.gomez.7e6@itb.cat>
2023-03-21 23:14:35 +0100

    Arreglo de funciones principales

--- obj/inmueble.js
+++ obj/inmueble.js
@@ -1,5 +1,5 @@
 class Inmueble {
-    
+
     constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, antiguedad) {
         this.direccion = direccion;
         this.mCuadrados = mCuadrados;
@@ -8,46 +8,55 @@ class Inmueble {
         this.estado = estado;
         this.coordenadas = coordenadas;
         this.antiguedad = antiguedad;
-        this.precio = this.calcularPrecio();
+        this.precio = precioBase;
     }
 
-    calcularPrecio() {
-        let precio = this.precioBase;
+    calcularPrecioPrincipal() {
 
-        if (this.antiguedad > 10) {
-            let descuento = (antiguedad - 10) * 0.01;
-            precio -= precio * descuento;
-        } else if (this.antiguedad > 20) {
-            precio -= precio * 0.1;
+        let precioCalc = this.precio;
+        let anyosCalc = this.antiguedad;
+        anyosCalc = anyosCalc - 10;
+
+        if (anyosCalc > 20) {
+            anyosCalc = 20;
+            precioCalc = precioCalc - (precioCalc * 0.01)
         }
-
-        return precio;
+        else if (anyosCalc <= 20 && anyosCalc >= 10) {
+            precioCalc = precioCalc - (precioCalc * 0.01)
+        }
+        return precioCalc;
     }
 }
-class Piso extends Inmueble{
-    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, antiguedad, planta, nHabitaciones, nLavabos, tieneAscensor, mCuadradosTerraza){
+
+
+class Piso extends Inmueble {
+    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, antiguedad, planta, puerta, nHabitaciones, nLavabos, tieneAscensor, mCuadradosTerraza) {
         super(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, antiguedad);
         this.planta = planta;
+        this.puerta = puerta
         this.nHabitaciones = nHabitaciones;
         this.nLavabos = nLavabos;
         this.tieneAscensor = tieneAscensor;
         this.mCuadradosTerraza = mCuadradosTerraza;
-        this.precio = this.calcularPrecio();
     }
 
     calcularPrecio() {
-        let precio = super.calcularPrecio();
-        if (this.tieneAscensor && this.piso >= 3) {
-          precio += precio * 0.03;
+        let precio = super.calcularPrecioPrincipal();
+        let precioBase = super.precioBase;
+
+        if (this.tieneAscensor == true && this.planta >= 3) {
+            precio += precioBase * 0.03;
         }
-        if (this.terraza) {
-          precio += this.mCuadradosTerraza * 300;
+        if (this.terraza == true) {
+            precio += this.mCuadradosTerraza * 300;
         }
         return precio;
     }
 }
-class Casa extends Inmueble{
-    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, tipoDeCasa, nHabitaciones, nLavabos, tieneJardin, tienePiscina, mCuadradosJardin){
+
+
+class Casa extends Inmueble {
+    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, tipoDeCasa, nHabitaciones, nLavabos, tieneJardin, tienePiscina, mCuadradosJardin) {
         super(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas);
         this.tipoDeCasa = tipoDeCasa;
         this.nHabitaciones = nHabitaciones;
@@ -58,61 +67,83 @@ class Casa extends Inmueble{
     }
 
     calcularPrecio() {
-        let precio = super.calcularPrecio();
-        if ((this.tieneJardin && this.mCuadrados > 100) && (this.mCuadrados <= 250)){
-          precio += precio * 0.05;
-        } else if (this.tieneJardin && this.mCuadrados > 250) {
-            precio += precio * 0.09;
-            if (this.tienePiscina) {
-                precio += precio * 0.04;
-              }
+
+        let precio = super.calcularPrecioPrincipal();
+        let precioBase = super.precioBase;
+
+        if (this.tieneJardin == true && this.mCuadrados > 100 && this.mCuadrados <= 250) {
+            precio += precioBase * 0.05;
+
+        }
+        else if (this.tieneJardin == true && this.mCuadrados > 250) {
+            precio += precioBase * 0.09;
+        }
+        if (this.tienePiscina == true) {
+            precio += precioBase * 0.04;
         }
         return precio;
     }
 }
-class Local extends Inmueble{
-    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, nVentanas, tieneVentanaMetalica){
+
+
+class Local extends Inmueble {
+    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, nVentanas, tieneVentanaMetalica) {
         super(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas);
         this.nVentanas = nVentanas;
         this.tieneVentanaMetalica = tieneVentanaMetalica;
     }
-
     calcularPrecio() {
-        let precio = super.calcularPrecio();
-        if (this.mCuadrados > 50){
-          precio += precio * 0.01;
-          if (this.nVentanas == 1 || this.nVentanas == 0) {
-            precio -= precio * 0.02;
-          } else if (this.nVentanas > 4) {
-            precio += precio * 0.02;
-          }
+        let precio = super.calcularPrecioPrincipal();
+        let precioBase = super.precioBase;
+
+        if (super.mCuadrados > 50) {
+            precio += precioBase * 0.01;
+        }
+        if (super.nVentanas == 1 || super.nVentanas == 0) {
+
+            precio -= precioBase * 0.02;
+        } else if (super.nVentanas > 4) {
+            precio += precioBase * 0.02;
         }
         return precio;
     }
 }
-class Comercio extends Local{
-    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, nVentanas, tieneVentanaMetalica, esAccesible){
+class Comercio extends Local {
+    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, nVentanas, tieneVentanaMetalica, esAccesible) {
         super(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, nVentanas, tieneVentanaMetalica);
         this.esAccesible = esAccesible;
     }
+
 }
-class Industrial extends Local{
-    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, nVentanas, tieneVentanaMetalica, tienePuestoDeCarga, tipoDeSuelo){
+class Industrial extends Local {
+    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, nVentanas, tieneVentanaMetalica, tienePuestoDeCarga, tipoDeSuelo) {
         super(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, nVentanas, tieneVentanaMetalica);
         this.tienePuestoDeCarga = tienePuestoDeCarga;
         this.tipoDeSuelo = tipoDeSuelo;
     }
 
     calcularPrecio() {
-        let precio = super.calcularPrecio();
-        if (this.tipoDeSuelo == "suelo urbano"){
-          precio += precio * 0.25;
+        let precio = super.calcularPrecioPrincipal();
+        let precioBase = super.precioBase;
+
+        if (super.mCuadrados > 50) {
+            precio += precioBase * 0.01;
+        }
+        if (super.nVentanas == 1 || super.nVentanas == 0) {
+
+            precio -= precioBase * 0.02;
+        } else if (super.nVentanas > 4) {
+            precio += precioBase * 0.02;
+        }
+
+        if(this.tipoDeSuelo == "urbano"){
+            precio = precio + precio * 0.25;
         }
         return precio;
     }
 }
-class Restauracion extends Local{
-    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, nVentanas, tieneVentanaMetalica, tieneExtractorDeHumos, tieneMobiliario, tieneCafetera){
+class Restauracion extends Local {
+    constructor(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, nVentanas, tieneVentanaMetalica, tieneExtractorDeHumos, tieneMobiliario, tieneCafetera) {
         super(direccion, mCuadrados, refCatastral, precioBase, estado, coordenadas, nVentanas, tieneVentanaMetalica);
         this.tieneExtractorDeHumos = tieneExtractorDeHumos;
         this.tieneMobiliario = tieneMobiliario;
